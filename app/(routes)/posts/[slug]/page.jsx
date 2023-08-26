70b480ed1fe83c93b82c91100dbbde57fb703181
Oskar Wojciechowski <oskar.woj@gmail.com>
2023-08-26 17:23:15 +0200

    dynamic route fix

---
+++ app/(routes)/posts/[slug]/page.jsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <section id="about-page" className=" h-screen  text-center flex">
+      <h1 className="m-auto font-waterfall text-6xl text-white-600 font-bold  ">
+        slugs
+      </h1>
+    </section>
+  );
+}
